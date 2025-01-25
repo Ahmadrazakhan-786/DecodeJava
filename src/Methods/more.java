1825b6ddb1d5024508f8a481c738a5663e6f2581
Ahmadraza <razakhan.srt786@gmail.com>
2025-01-25 22:39:09 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Methods/more.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class more {
+    public static void fun(){
+        System.out.println("Asslamo Aalikum ");
+    }
+    public static void main(String[] args) {
+        fun();
+    }
+}
