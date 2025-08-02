02183e276533dc0a763970be2311cf6bfc6b2f6c
Ahmadraza <razakhan.srt786@gmail.com>
2025-08-02 21:02:33 +0530

    Revision

---
+++ src/LinkedList/Revision.java
@@ -0,0 +1,46 @@
+package LinkedList;
+
+class RLL{
+    int val;
+    Node next;
+    Node head;
+    Node tail;
+    int size;
+
+
+    void insertAtEnd(int val){
+
+        Node temp = new Node(val);
+
+        if (head == null) {
+            head = temp;
+            tail = temp;
+            return;
+        }
+        else {
+            tail.next = temp;
+            tail = temp;
+        }
+    }
+
+    void display(){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+}
+public class Revision {
+    public static void main(String[] args) {
+        RLL list = new RLL();
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(40);
+        list.insertAtEnd(50);
+        list.display();
+
+    }
+}
