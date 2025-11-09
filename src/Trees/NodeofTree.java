7e9295ca79424b1fc0365e1bf973bbe4c9da041d
Ahmadraza <razakhan.srt786@gmail.com>
2025-11-09 19:48:13 +0530

    Tree

---
+++ src/Trees/NodeofTree.java
@@ -0,0 +1,43 @@
+package Trees;
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val = val;
+    }
+}
+public class NodeofTree {
+    public static void main(String[] args) {
+        Node a = new Node(1); // root node is a here
+        Node b = new Node(4);
+        Node c = new Node(3);
+        Node d = new Node(2);
+        Node e = new Node(6);
+        Node f = new Node(5);
+
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.right = f;
+        Node g = new Node(10);
+        Node h = new Node(20);
+
+        c.left = g;
+        e.right = h;
+
+        a.left = null; // left subtree ka connection tod diya
+
+//        System.out.println(b.val); // 4
+//        System.out.println(a.left.val); // 4
+//        System.out.println(e.val); // 6
+//        System.out.println(a.left.right.val); // 6
+
+        display(a);
+    }
+    private static void display(Node root){
+        if (root==null) return; // base case
+        System.out.print(root.val + " ");// khud ki value print kardi
+        display(root.left); // recursion ki help se left subtree display ho jaega
+        display(root.right);// recursion ki help se right subtree display ho jaega
+    }
+}
