d15c0ae4b1ba6ba1273b232fcc01b78cd7eef30e
Ahmadraza <razakhan.srt786@gmail.com>
2026-01-21 19:15:56 +0530

    K Sorted Array

---
+++ src/Heaps/KsortedArray.java
@@ -0,0 +1,28 @@
+package Heaps;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class KsortedArray { // TC -> O(nlogk)
+    public static void main(String[] args) {
+        int[] arr = {6,5,3,2,8,10,9};
+        int k = 3;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int ele : arr){
+            pq.add(ele);
+            if (pq.size()>k) {
+                int x = pq.remove();
+                ans.add(x);
+            }
+        }
+        while (pq.size()>0) {
+            int x = pq.remove();
+            ans.add(x);
+        }
+        System.out.print(ans);
+
+
+
+    }
+}
