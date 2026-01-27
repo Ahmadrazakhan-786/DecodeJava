a64c29ea30c0c15243e4551ed2d45ae411f27907
Ahmadraza <razakhan.srt786@gmail.com>
2026-01-27 22:58:05 +0530

    Min Heap Implementation using Array removal

--- src/Heaps/MinHeapImplementation.java
+++ src/Heaps/MinHeapImplementation.java
@@ -33,6 +33,25 @@ class Minheap{
     public int size(){
         return size; // size of the array
     }
+    public int remove(){
+        if (size==0) System.out.print("Heap is empty");
+        int peek = arr[0];
+        swap(0,size-1);
+        size--; // removal
+        downHeapify(0); // 0 isliye pass kiya qki swap ke baad first ele se niche aaega
+        return peek;
+    }
+    public void downHeapify(int parent){
+        if (parent>=size-1) return; // base case
+        int lc = 2*parent+1; // left child
+        int rc = 2*parent+2; // right child
+        int minIdx = parent; // let ki avi lc,rc aur parent me sbse min parent hai
+        if (lc<size && arr[lc]<arr[minIdx]) minIdx = lc; // iska matlab agr lc present hoto and  lc parent se chota hua to minIdx ko update krke lc kardo
+        if (rc<size && arr[rc]<arr[minIdx]) minIdx = rc;
+        if (parent == minIdx) return;
+        swap(parent,minIdx); // fir swap kardo unke children ke smallest value se
+        downHeapify(minIdx);
+    }
 }
 public class MinHeapImplementation {
     public static void main(String[] args) {
@@ -40,11 +59,15 @@ public class MinHeapImplementation {
         pq.add(1);
         pq.add(6);
         pq.add(2);
-        System.out.println(pq.size());
-        pq.add(0);
         System.out.println(pq.peek());
-        pq.add(-4);
+        pq.remove();
         System.out.println(pq.peek());
+
+//        System.out.println(pq.size());
+//        pq.add(0);
+//        System.out.println(pq.peek());
+//        pq.add(-4);
+//        System.out.println(pq.peek());
 
     }
 }
